8968f6d27158653c7920fdc37c8a24774478e449
azlan <azlanirshad@gmail.com>
2024-10-21 19:24:17 -0400

    to-do list

---
+++ list.py
@@ -0,0 +1,36 @@
+task_list = []
+
+def add(task_list):
+    while True:
+        task_name = input("Enter the task to add: ")
+        task_list.append(task_name)
+        print("current task list:", task_list)
+        choice = input("Do you want to add more tasks? (y / n)")
+        if choice.lower() == "n":
+            break
+
+def view(task_list):
+    task_list.sort()
+    task_name = input("Enter the task to view: ")
+    if task_name in task_list:
+        print("task present!")
+    else:
+        print("task not present")
+        task_list.append(task_name)
+    print("updated task list:", task_list)
+
+def remove(task_list):
+    task_name = input("Enter the task name to remove: ")
+    if task_name in task_list:
+        task_list.remove(task_name)
+        print("task removed:", task_name)
+        print("new task list", task_list)
+    else:
+        print("task not there!")
+        print("List Unchanged:", task_list)
+
+
+add(task_list)
+view(task_list)
+remove(task_list)
+
